8eeaeb4b41393618d363f5b5ab633d1bcd37b320
gtorres777 <51068028+gtorres777@users.noreply.github.com>
2022-06-28 22:26:42 -0500

    Update deploy_new_clients_eks.sh

--- deploy_new_clients_eks.sh
+++ deploy_new_clients_eks.sh
@@ -18,9 +18,9 @@ fi
 # Creating PostgreSQL User
 
 
-PGPASSWORD="$DB_ENV_POSTGRES_PASSWORD" /usr/bin/psql -X -A --quiet --host $DB_PORT_5432_TCP_ADDR --port=5432 --username="$DB_ENV_POSTGRES_USER" -t -c "CREATE USER \"$psql_user\" WITH PASSWORD '$psql_user_password'; "
+PGPASSWORD="$DB_MASTER_ENV_POSTGRES_PASSWORD" /usr/bin/psql -X -A --quiet --host $DB_PORT_5432_TCP_ADDR --port=5432 --username="$DB_MASTER_ENV_POSTGRES_USER" -t -c "CREATE USER \"$psql_user\" WITH PASSWORD '$psql_user_password'; "
 error=$?; if [ $error -eq 0 ]; then echo "Postgres User created Succesfully"; else echo "ERROR: $error"; fi
 
 
-PGPASSWORD="$DB_ENV_POSTGRES_PASSWORD" /usr/bin/psql -X -A --quiet --host $DB_PORT_5432_TCP_ADDR --port=5432 --username="$DB_ENV_POSTGRES_USER" -t -c "ALTER ROLE \"$psql_user\" WITH createdb; "
+PGPASSWORD="$DB_MASTER_ENV_POSTGRES_PASSWORD" /usr/bin/psql -X -A --quiet --host $DB_PORT_5432_TCP_ADDR --port=5432 --username="$DB_MASTER_ENV_POSTGRES_USER" -t -c "ALTER ROLE \"$psql_user\" WITH createdb; "
 error=$?; if [ $error -eq 0 ]; then echo "Succesfully Altered Role with CREATEDB"; else echo "ERROR: $error"; fi
